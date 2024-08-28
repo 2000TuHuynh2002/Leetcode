ef90e4ab27128dcb51ec10d87705cc06b5f11df7
Tu Huynh <huynhlethanhtu@gmail.com>
2024-08-28 20:07:54 +0700

    0001. Two Sum

---
+++ 0001.Two_sum/main.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+
+        """
+        for idx, num in enumerate(nums):
+            i = 1
+            while idx+i < len(nums):
+                if nums[idx]+nums[idx+i] == target:
+                    return [idx, idx+i]
+                i += 1
