8400371c097f1cead7df11309592db888dc04830
Tu Huynh <huynhlethanhtu@gmail.com>
2024-09-03 19:46:09 +0700

    0570. Managers with at Least 5 Direct Reports

---
+++ 0570.Managers_with_at_least_5_direct_reports/main.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT E1.name
+FROM Employee E1, (SELECT managerId
+					FROM Employee
+					GROUP BY managerId
+					HAVING COUNT(managerId)>=5) E2
+WHERE E1.id=E2.managerId
