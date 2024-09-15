4d6f6b85d608a9c0a7cc472e8545fdd7feb81b95
Tu Huynh <huynhlethanhtu@gmail.com>
2024-09-15 21:02:11 +0700

    1436. Destination City

---
+++ 1436.Destination_city/main.py
@@ -0,0 +1,11 @@
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        for path in paths:
+            destination = path[1]
+            i = 0
+            for path2 in paths:
+                if path[1] == path2[0]:
+                    break
+                i += 1
+            if i == len(paths):
+                return destination
