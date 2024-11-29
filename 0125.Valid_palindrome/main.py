a685c09c88415a4163fedbf0e5f005631c993572
Tu Huynh <huynhlethanhtu@gmail.com>
2024-11-29 08:55:35 +0700

    0125. Valid Palindrome

---
+++ 0125.Valid_palindrome/main.py
@@ -0,0 +1,9 @@
+import re
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned_string = re.sub(r'[^A-Za-z0-9]', '', s)
+        lower_cleaned_string = cleaned_string.lower()
+
+        return lower_cleaned_string == lower_cleaned_string[::-1]
