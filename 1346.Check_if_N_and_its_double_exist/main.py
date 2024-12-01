2627b88e1ebca9a18c3feec760d758382bc3dbef
Tu Huynh <huynhlethanhtu@gmail.com>
2024-12-01 22:04:05 +0700

    1346. Check If N and Its Double Exist

---
+++ 1346.Check_if_N_and_its_double_exist/main.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        for idx, _ in enumerate(arr):
+            i = 1
+            while idx+i < len(arr):
+                if arr[idx] == arr[idx+i] * 2 or arr[idx] == arr[idx+i] / 2:
+                    return True
+                i += 1
+        return False
