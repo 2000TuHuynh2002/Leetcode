0bc4a00c0a6062510c496aefa404517e66d73bdb
Tu Huynh <huynhlethanhtu@gmail.com>
2024-12-01 22:46:37 +0700

    171. Excel Sheet Column Number

---
+++ 0171.Excel_sheet_column_number/main.py
@@ -0,0 +1,9 @@
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        result = 0
+        excel_gap = 26
+
+        for char in columnTitle:
+            result = result*excel_gap + (ord(char) - ord("A") + 1)
+
+        return result
