22e6ee6c284221e86a855c3088935e1568bcab6e
Tu Huynh <huynhlethanhtu@gmail.com>
2024-12-03 13:50:24 +0700

    7. Reverse Integer

---
+++ 0007.Reverse_integer/main.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        range_min = -2**31
+        range_max = 2**31 - 1
+
+        x_str_reversed = str(abs(x))[::-1]
+
+        if x >= 0:
+            result = int(x_str_reversed)
+        else:
+            result = -int(x_str_reversed)
+
+        if result < range_min or result > range_max:
+            return 0
+        else:
+            return result
