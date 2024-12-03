7c1c5fd432e32f3953d2066e926520dd0a202370
Tu Huynh <huynhlethanhtu@gmail.com>
2024-12-03 22:19:03 +0700

    620. Not Boring Movies

--- 1934.Confirmation_rate/main.sql
+++ 1934.Confirmation_rate/main.sql
@@ -4,8 +4,8 @@ SELECT
   s.user_id,
   ROUND(AVG(IF(c.action = "confirmed", 1, 0)), 2) AS confirmation_rate
 FROM
-  signups s
+  Signups s
 LEFT JOIN
-  confirmations c ON s.user_id = c.user_id
+  Confirmations c ON s.user_id = c.user_id
 GROUP BY
   s.user_id;
