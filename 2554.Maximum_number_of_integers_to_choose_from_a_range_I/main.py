bf7868799ac7e8a55ba6b8b404de6242ca086d20
Tu Huynh <huynhlethanhtu@gmail.com>
2024-12-06 08:52:32 +0700

    2554. Maximum Number of Integers to Choose From a Range I

---
+++ 2554.Maximum_number_of_integers_to_choose_from_a_range_I/main.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxCount(self, banned: List[int], n: int, maxSum: int) -> int:
+        # Use set for faster lookup
+        banned = set(banned)
+
+        result_count = 0
+        temp_sum = 0
+
+        for idx in range(1, n+1):
+            if idx in banned:
+                continue
+
+            temp_sum += idx
+            if temp_sum > maxSum:
+                break
+            else:
+                result_count += 1
+
+        return result_count
