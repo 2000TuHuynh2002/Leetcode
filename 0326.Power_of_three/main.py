39e2763b4da97542a4a96d5f9742a3cc339a9c75
Tu Huynh <huynhlethanhtu@gmail.com>
2025-08-13 13:35:25 +0700

    0326. Power of Three

---
+++ 0326.Power_of_three/main.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        i = 1
+        while (i <= n):
+            if i == n:
+                return True
+            i *= 3
+        return False
