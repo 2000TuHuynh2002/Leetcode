6d5c97aba62e0040cba34153ebf10c90a6ddd1a4
Tu Huynh <huynhlethanhtu@gmail.com>
2025-08-16 08:48:28 +0700

    1323. Maximum 69 Number

---
+++ 1323.Maximum_69_number/main.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximum69Number(self, num: int) -> int:
+        num_str = str(num)
+        result = ""
+        for idx, s in enumerate(num_str):
+            if num_str[idx] == "6":
+                result += "9" + num_str[idx+1:]
+                return int(result)
+            result += s
+        return int(result)
