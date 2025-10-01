edc6ecc7f75dc1c5f91472dbec605a2cee85d026
Tu Huynh <huynhlethanhtu@gmail.com>
2025-10-01 15:14:51 +0700

    1518. Water Bottles

---
+++ 1518.Water_bottles/main.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        numBottlesConsumed = 0
+
+        while numBottles >= numExchange:
+            k = numBottles // numExchange
+            numBottlesConsumed += numExchange * k
+            numBottles -= numExchange * k
+            numBottles += k
+
+        return numBottlesConsumed + numBottles
