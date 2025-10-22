089fa59b7ce1affacf72abfc91e828b7b978c4b6
Tu Huynh <huynhlethanhtu@gmail.com>
2025-10-22 08:30:22 +0700

    1716. Calculate Money in Leetcode Bank

---
+++ 1716.calculate_money_in_Leetcode_bank/main.py
@@ -0,0 +1,15 @@
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        count = 0
+        day_money = 0
+        
+        for i in range (1, n+1):
+            c = i % 7
+            if c == 0:
+                count += day_money + 7
+            else:
+                count += day_money + c
+            if  c == 0:
+                day_money += 1
+        
+        return count
