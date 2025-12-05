c02505a575f48ef1699629281a1cda5721105985
Tu Huynh <huynhlethanhtu@gmail.com>
2025-12-05 14:50:00 +0700

    3432. Count Partitions with Even Sum Difference

---
+++ 3432.count_partitions_with_even_sum_difference/countPartitions.go
@@ -0,0 +1,22 @@
+package countPartitions
+
+func sumArray(numbers []int) int {
+	result := 0
+	for i := 0; i < len(numbers); i++ {
+		result += numbers[i]
+	}
+	return result
+}
+
+func countPartitions(nums []int) int {
+	count := 0
+	numsLength := len(nums)
+
+	for i := 1; i < numsLength; i++ {
+		if (sumArray(nums[:i])+sumArray(nums[i:numsLength]))%2 == 0 {
+			count += 1
+		}
+	}
+
+	return count
+}
